e95df2409c3da2940a81d8b7daed5ece52714e44
mattjuskiw <juskiwmatt@gmail.com>
2019-11-18 20:27:02 -0500

    Added some user registration functionality, updated styling, initialized login.js

--- controllers/api-users.js
+++ controllers/api-users.js
@@ -4,9 +4,21 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 module.exports = function(app) {
-	app.post("/signup", (req,res) => {
+	app.post("/signup", async(req,res) => {
 		console.log(req.body);
+
+		try {
+			var password = await bcrypt.hash(req.body.password,10);
+			db.Users.create({
+				name: req.body.name,
+				username: req.body.username,
+				email: req.body.email,
+				password_hash: password
+			}).then((newUser) => {
+				res.json(newUser);
+			});
+		} catch {
+			res.redirect("/");
+		}
 	});
-
-
 };
--- models/users.js
+++ models/users.js
@@ -1,10 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
     var Users = sequelize.define("Users", {
+        name: {
+            type: DataTypes.STRING
+        },
         username: {
             type: DataTypes.STRING
         },
         password_hash: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING
         },
         email: {
             type: DataTypes.STRING
